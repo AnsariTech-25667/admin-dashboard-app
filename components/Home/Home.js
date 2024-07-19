0982825368270dbdf6c981ebab71ff4f4d503f7b
Ansari Maaz <159632328+AnsariTech-25667@users.noreply.github.com>
2024-07-19 06:02:38 +0530

    Update Home.js

---
+++ components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to User App</h1>
+      {/* UI based on Figma design */}
+    </div>
+  );
+};
+
+export default Home;
