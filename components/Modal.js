f525d0a675d7f74cd559ab4fd1793b7743660a7f
Ansari Maaz <159632328+AnsariTech-25667@users.noreply.github.com>
2024-07-19 07:20:35 +0530

    Update Modal.js

--- components/Modal.js
+++ components/Modal.js
@@ -1,1 +1,27 @@
 
+// components/Modal/Modal.js
+import React from 'react';
+import { Modal, ModalHeader, ModalBody, ModalFooter, Button } from '@nextui-org/react';
+
+const CustomModal = ({ isOpen, toggle, title, children, onConfirm }) => {
+  return (
+    <Modal open={isOpen} onClose={toggle}>
+      <ModalHeader>
+        <h4>{title}</h4>
+      </ModalHeader>
+      <ModalBody>
+        {children}
+      </ModalBody>
+      <ModalFooter>
+        <Button auto flat color="error" onClick={toggle}>
+          Cancel
+        </Button>
+        <Button auto onClick={onConfirm}>
+          Confirm
+        </Button>
+      </ModalFooter>
+    </Modal>
+  );
+};
+
+export default CustomModal;
